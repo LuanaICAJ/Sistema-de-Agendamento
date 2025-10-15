64f202613260e036ed5e7bb5aa8f2e16e30d939f
Luana Neves <luananeves@pop-os.localdomain>
2025-10-15 11:50:49 -0300

    testes atualizados

--- app/tests/test_edit_delet.py
+++ app/tests/test_edit_delet.py
@@ -1,6 +1,6 @@
 import pytest
 from django.urls import reverse
-from app.models import ReservaEquipamento
+from app.models import ReservaEquipamento, ReservaSala
 
 @pytest.mark.django_db
 def test_editar_reserva_equipamento(client, equipamento):
@@ -31,4 +31,36 @@ def test_deletar_reserva_equipamento(client, equipamento):
     url = reverse('deletar_reserva_equipamento', args=[reserva.pk])
     response = client.post(url, follow=True)
     assert ReservaEquipamento.objects.count() == 0
+    assert response.status_code == 200
+
+# edição de sala
+@pytest.mark.django_db
+def test_editar_reserva_sala(client, sala):
+    reserva = ReservaSala.objects.create(
+        sala= sala,
+        data='2025-10-15',
+        hora_inicio='08:00',
+        hora_fim='09:00',
+        status='confirmado'
+    )
+    url = reverse('editar_reserva_sala', args=[reserva.pk])
+    data = {'data': '2025-10-16', 'hora_inicio': '10:00', 'hora_fim': '11:00'}
+    response = client.post(url, data, follow=True)
+    reserva.refresh_from_db()
+    assert reserva.data.strftime('%Y-%m-%d') == '2025-10-16'
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_deletar_reserva_sala(client, sala):
+    reserva = ReservaSala.objects.create(
+        sala= sala,
+        data='2025-10-15',
+        hora_inicio='08:00',
+        hora_fim='09:00',
+        status='confirmado'
+    )
+    url = reverse('deletar_reserva_sala', args=[reserva.pk])
+    response = client.post(url, follow=True)
+    assert ReservaSala.objects.count() == 0
     assert response.status_code == 200
